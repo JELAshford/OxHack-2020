d2844b9ee30f34f038f72f7afcd86df70ab0ef42
James Ashford <james.ashford@bnc.ox.ac.uk>
2020-11-15 12:59:20 +0000

    Updated label sizes and colours based on level

--- James_IdeaTesting/TwitterVisualiser/coocurrance_plot.py
+++ James_IdeaTesting/TwitterVisualiser/coocurrance_plot.py
@@ -29,6 +29,8 @@ def dice_significance(cooc_array, word, word_list):
 # Define project path
 PROJ_PATH = "/Users/jamesashford/Documents/Projects/Hackathons/Oxford Hack 2020/OxHack-2020/James_IdeaTesting/TwitterVisualiser/"
 NUM_OF_COOCS = 10
+SEARCH_TERM = 'Climate Change'
+ALL_SEARCH_TERMS = TextBlob(SEARCH_TERM).words
 
 # Sort out fonts
 font_files = font_manager.findSystemFonts(fontpaths=f"{PROJ_PATH}rsc")
@@ -36,8 +38,7 @@ font_list = font_manager.createFontList(font_files)
 font_manager.fontManager.ttflist.extend(font_list)
 
 # Read in tweets
-search_term = 'Climate Change'
-tweet_file = pd.read_csv(f"{PROJ_PATH}rsc/{search_term}_tweets.csv")
+tweet_file = pd.read_csv(f"{PROJ_PATH}rsc/{SEARCH_TERM}_tweets.csv")
 tweets = tweet_file["Tweet"].dropna().values
 
 # Extract and clean words
@@ -45,7 +46,7 @@ all_words = TextBlob(" ".join(tweets).upper()).words.singularize().lemmatize()
 # Get stop-words
 stop_words = list(set(stopwords.words('english'))) + ['thi']
 # Remove Stop and Short Words
-words = [w for w in all_words if len(w) > 2 and w.lower() not in stop_words]
+words = [w for w in all_words if len(w) > 3 and w.lower() not in stop_words]
 
 # Remove words that only occur once
 counts = dict(Counter(words))
@@ -57,14 +58,15 @@ dtm = np.array([[1 if (tweet.upper().count(word) > 0) else 0 for word in key_wor
 cooc = np.dot(dtm.T, dtm)
 
 graph_data = []
-tier2_names = []
-for term in TextBlob(search_term).words:
+layer1_names = []
+layer2_names = []
+for term in ALL_SEARCH_TERMS:
     # Statistical significance of search_term
     term_dice_stats = dice_significance(cooc, term, key_words)
     term_dice_stats = dict_value_sort(term_dice_stats)
     # Get NUM_OF_COOCS most similar words
     most_similar = list(term_dice_stats.keys())[0:NUM_OF_COOCS]
-    tier2_names += most_similar
+    layer1_names += most_similar
     # Create a structure to hold the node links
     graph_data += [{"from":term.upper(), "to":set_name, "stat":term_dice_stats[set_name]} for set_name in most_similar]
     # Iterate over each of the chosen coocs, and find their closest
@@ -74,6 +76,7 @@ for term in TextBlob(search_term).words:
         word_dice_stats = dict_value_sort(word_dice_stats)
         # Choose top nearby matches
         top_neighbours = list(word_dice_stats.keys())[0:10]
+        layer2_names += top_neighbours
         new_graph_data = [{"from":word.upper(), "to":set_name, "stat":word_dice_stats[set_name]} for set_name in top_neighbours]
         # Add to existing graph data
         graph_data += new_graph_data
@@ -83,30 +86,45 @@ gd = pd.DataFrame.from_dict(graph_data)
 # Create co-occurance graph
 # G = nx.from_numpy_matrix(cooc)
 G = nx.from_pandas_edgelist(gd, "from", "to", "stat")
-pos = nx.nx_agraph.graphviz_layout(G, prog='fdp')
 
 # Generate colours
 colours, sizes = [], []
+l0, l1, l2 = {}, {}, {}
 for node in G:
-    if node in TextBlob(search_term).words.upper():
-        col = 'red'
-        size = 2000
-    elif node in tier2_names:
-        col = 'orange'
-        size = 500
+    if node in ALL_SEARCH_TERMS.upper():
+        col = 'darkblue' #'red'
+        size = counts[node]*1000 #5000
+        l0[node] = node
+    elif node in layer1_names:
+        col = 'lightblue' #'orange'
+        size = counts[node]*1000 #2500
+        l1[node] = node
     else:
-        col = 'blue'
-        size = 100
+        col = 'cyan' #'blue'
+        size = counts[node]*100 #1000
+        l2[node] = node
     colours.append(col)
     sizes.append(size)
 
 # Visualisation
 fig = plt.figure(figsize=(15, 10))
-nx.draw_networkx(G, pos, with_labels=True, alpha=0.5, node_color=colours, node_size = sizes, font_family="Swiss911 UCm BT", font_size=18)
-nx.draw_networkx_labels(G, pos, font_family="Swiss911 UCm BT", font_size=18)
+pos = nx.spring_layout(G)
+if len(ALL_SEARCH_TERMS) == 1:
+    pos = nx.nx_agraph.graphviz_layout(G, prog='twopi')
+# Draw edges
+edges = nx.draw_networkx_edges(G, pos, alpha=1, width=2, edge_color='white') #width=gd["stat"].values*10)
+# Draw nodes, once white for background then again with colour+alpha
+nodes = nx.draw_networkx_nodes(G, pos, alpha=1, node_color='white', node_size=sizes)
+nodes = nx.draw_networkx_nodes(G, pos, alpha=0.8, node_color=colours, node_size=sizes)
+nodes.set_edgecolor('black')
+# Draw labels for each layer, with proportional sizes
+labels0 = nx.draw_networkx_labels(G, pos, labels=l0, font_family="Swiss911 UCm BT", font_size=60)
+labels1 = nx.draw_networkx_labels(G, pos, labels=l1, font_family="Swiss911 UCm BT", font_size=30)
+labels2 = nx.draw_networkx_labels(G, pos, labels=l2, font_family="Swiss911 UCm BT", font_size=22, font_color="white")
+labels2 = nx.draw_networkx_labels(G, pos, labels=l2, font_family="Swiss911 UCm BT", font_size=21, font_color="black")
 
 # Save
-save_name = f"{PROJ_PATH}output/{search_term}_coocgraph.png"
+save_name = f"{PROJ_PATH}output/{SEARCH_TERM}_coocgraph.png"
 plt.savefig(save_name, dpi=300, transparent=True)
 
 # Show
